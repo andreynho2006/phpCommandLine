a62e06ee30243801ae61aaa4d109dc81a0fe02eb
andreynho2006 <andreynho2006@gmail.com>
2018-08-16 19:09:08 +0300

    add AddCommand adapter to the app

--- tasks.php
+++ tasks.php
@@ -4,10 +4,38 @@
 
 use Symfony\Component\Console\Application;
 
+
 require 'vendor/autoload.php';
 
 $app = new Application('Task App', '1.0');
 
-$app->add(new Acme\ShowCommand);
+
+//connect to database
+try 
+{
+    $pdo = new PDO('sqlite:db.sqlite');
+
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $exception) 
+{
+
+    echo 'Could not connect to the database';
+    exit(1);
+
+}
+
+
+//pass connection to adapter
+$dbadapter = new Acme\DatabaseAdapter($pdo);
+
+
+//pass adapter to show command
+$app->add(new Acme\ShowCommand($dbadapter));
+
+//pass adapter to add command
+$app->add(new Acme\AddCommand($dbadapter));
 
 $app->run();
